d877edfe68ffaa752207efcf9df0c3231e926b94
Jonathan B <dj.arvore@gmail.com>
2024-04-08 16:21:33 -0400

    test
    fist

---
+++ database/cliente.py
@@ -0,0 +1,9 @@
+CLIENTES =[
+    
+    {'id': 1, 'nome' : 'joh', 'email' : 'test@test.com'},
+    {'id': 2, 'nome' : 'maria', 'email' : 'ttest@test.com'},
+    {'id': 3, 'nome' : 'care', 'email' : 'testt@test.com'},
+    {'id': 4, 'nome' : 'vulgo', 'email' : 'teeest@test.com'},
+    {'id': 5, 'nome' : 'tere', 'email' : 'teeeeest@test.com'},
+    
+]
